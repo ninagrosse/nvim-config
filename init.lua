415fb2151816953ef9a572b888a9a3e36616b580
Nina Große <nina.grosse@posteo.de>
2025-01-05 12:40:40 +0100

    Disable Lua_LS 'missing-fields' warnings

--- init.lua
+++ init.lua
@@ -639,7 +639,7 @@ require('lazy').setup({
                 callSnippet = 'Replace',
               },
               -- You can toggle below to ignore Lua_LS's noisy `missing-fields` warnings
-              -- diagnostics = { disable = { 'missing-fields' } },
+              diagnostics = { disable = { 'missing-fields' } },
             },
           },
         },
